2f703d1f2e4135e2b44d2b0773160bcfeea825bc
rodrigoaqm10 <144729628+rodrigoaqm10@users.noreply.github.com>
2025-08-19 12:40:07 -0400

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,7 +7,7 @@ export default function Landing() {
     <div className="container" style={{minHeight:'100dvh', display:'grid', placeItems:'center'}}>
       <div style={{textAlign:'center'}}>
         <div style={{fontSize:72, fontWeight:900, lineHeight:1, marginBottom:12, letterSpacing:-1}}>Pokedex</div>
-        <div style={{opacity:.8, marginBottom:28}}>Explore and learn about Pokémon using the public PokeAPI</div>
+        <div style={{opacity:.8, marginBottom:28}}>Explota y aprende sobre los Pokemones usando la PokeAPI pública</div>
         <button className="btn" onClick={()=>navigate('/grid')}>START</button>
       </div>
     </div>
