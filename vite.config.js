e468718971cf0cacf87bda2489b90f36c330b51a
rodrigoaqm10 <144729628+rodrigoaqm10@users.noreply.github.com>
2025-08-19 12:26:09 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.js'
-  }
+  base: '/pinflag-poke/', // <-- usa /<tu-repo>/
+  test: { environment: 'jsdom', setupFiles: './vitest.setup.js' }
 })
